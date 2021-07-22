ce7655ec975e6cc97a09fb47fa132600c0908128
CaptDread <domlycaon@gmail.com>
2021-07-22 15:40:54 -0400

    fin

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,33 +1,9 @@
 "use strict";
 
-var showcaseImg = document.querySelector('.showcaseImg');
-var img1 = "dist/img/plumb1.jpg";
-var img2 = "dist/img/plumb2.jpg";
-var img3 = "dist/img/plumb3.jpg"; // var waypoint = new Waypoint({
-//     element: document.getElementsByClassName('plumbus_hero-cta1'),
-//     handler: function(direction) {
-//         console.log('Scrolled to waypoint!')
-//     }
-// })
-
-var galleryShow = function galleryShow() {
-  console.log("Gallery Starting");
-  setInterval(function () {
-    console.log("Gallery Started");
-    setTimeout(function () {
-      showcaseImg.src = img2;
-      console.log("img2");
-    }, 1500);
-    setTimeout(function () {
-      showcaseImg.src = img3;
-      console.log("img3");
-    }, 1500);
-    setTimeout(function () {
-      showcaseImg.src = img1;
-      console.log("img1");
-    }, 1500);
-  }, 45000);
-};
-
-galleryShow();
+$('.panel-collapse').on('show.bs.collapse', function () {
+  $(this).siblings('.panel-heading').addClass('active');
+});
+$('.panel-collapse').on('hide.bs.collapse', function () {
+  $(this).siblings('.panel-heading').removeClass('active');
+});
 //# sourceMappingURL=main.js.map
